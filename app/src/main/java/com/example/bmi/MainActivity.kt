d82ad853ad10ed3c6561d6b42e6f8d2c8895d5b1
shivanshu11092003 <148114391+shivanshu11092003@users.noreply.github.com>
2024-01-18 01:16:27 +0530

    color update

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.bmi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.bmi.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -24,13 +26,48 @@ class MainActivity : AppCompatActivity() {
             val height=cm.text.toString()
             val bmi=weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
 
-            final.text=bmi.toString()
+            val a= String.format("%.2f",bmi).toFloat()
+
+            final.text=a.toString()
+
+            colour(bmi)
 
 
 
 
         }
 
+
+
+    }
+
+    private fun colour(bmi: Float) {
+        val bmitext=findViewById<TextView>(R.id.Colour)
+
+        var finaltext=""
+        var color = 0
+        when{
+            bmi>=30 ->{
+                finaltext="Obese"
+                color=R.color.red
+            }
+            bmi in 25.0..29.99 ->{
+                finaltext = "Overweight"
+                color=R.color.orange
+
+            }
+            bmi in 18.5..24.0 ->{
+                finaltext="Normal"
+                color=R.color.Green
+            }
+            bmi<18.5 ->{
+                finaltext="UnderWeight"
+                color=R.color.yellow
+            }
+
+        }
+        bmitext.setTextColor(ContextCompat.getColor(this,color))
+        bmitext.text=finaltext
 
 
     }
